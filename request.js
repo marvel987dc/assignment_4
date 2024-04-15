120d0596609febc427fc58fd4afe0aef7ef1b80f
Juan <69006778+marvel987dc@users.noreply.github.com>
2024-04-14 22:00:04 -0400

    Add files via upload

---
+++ request.js
@@ -0,0 +1,17 @@
+// 'use strict';
+
+// const url =  'https://soundcloudstefan-skliarovv1.p.rapidapi.com/addTrackComment';
+
+
+// const CreateRequest = function(url){
+//       const httpRequest = new XMLHttpRequest(url);
+//       httpRequest.addEventListener('readystatechange', (url) => {
+//           if (httpRequest.readyState === 4) {
+//               console.log(httpRequest.responseText);
+//           }
+//       }); 
+//       httpRequest.open('POST', url);
+//       httpRequest.send();
+// }
+// CreateRequest(url);
+
